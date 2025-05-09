6cf84422555ff379fabffc0c68023365c338a38c
Poramate <poramate.pro@gmail.com>
2025-05-09 16:14:15 +0700

    fjx athu

--- middlewares/product.js
+++ middlewares/product.js
@@ -7,13 +7,13 @@ const router = express.Router();
 
 //Add product
 router.post("/product-add", authUser ,addProduct)
-router.get("/product-get", authUser , getProduct)
+router.get("/product-get" , getProduct)
 router.get("/product-get/:userId", authUser, getProductByUserId)
 router.put("/product-put/:id", authUser , editByPutProduct)
 router.delete("/product-delete/:id", authUser , deleteProduct)
 
 //Get all auction product
-router.get("/product-get-auction", authUser, getAllAuctionProduct);
+router.get("/product-get-auction", getAllAuctionProduct);
 
 router.get("/my-products", authUser, async (req, res) => {
     const { _id } = req.user;
