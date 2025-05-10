1f7e5a5f221b8f799f466dff12535b59124387af
Nutty <tanut.ktr@gmail.com>
2025-05-10 22:43:32 +0700

    add product middleware get product by genre and get product by productId

--- middlewares/product.js
+++ middlewares/product.js
@@ -1,37 +1,70 @@
 import express from "express";
-import { addProduct, getProduct , getProductByUserId , editByPutProduct , deleteProduct, getAllAuctionProduct , getProductById} from "../controller/productController.js";
+import {
+  addProduct,
+  getProduct,
+  getProductByUserId,
+  editByPutProduct,
+  deleteProduct,
+  getAllAuctionProduct,
+  getProductById,
+  getOnceProductById,
+} from "../controller/productController.js";
 
-import {authUser} from "./authMiddleware.js";
+import { authUser } from "./authMiddleware.js";
 import { Product } from "../model/Product.js";
 
 const router = express.Router();
 
 //Add product
-router.post("/product-add", authUser ,addProduct)
-router.get("/product-get", authUser , getProduct)
-router.get("/product-get/:id", authUser , getProductById)
-router.get("/product-get/:userId", authUser, getProductByUserId)
-router.put("/product-put/:id", authUser , editByPutProduct)
-router.delete("/product-delete/:id", authUser , deleteProduct)
+router.post("/product-add", authUser, addProduct);
+router.get("/product-get", authUser, getProduct);
+router.get("/product/:id", getOnceProductById);
+router.get("/product-get/:id", authUser, getProductById);
+router.get("/product-get/:userId", authUser, getProductByUserId);
+router.put("/product-put/:id", authUser, editByPutProduct);
+router.delete("/product-delete/:id", authUser, deleteProduct);
 
 //Get all auction product
 router.get("/product-get-auction", authUser, getAllAuctionProduct);
 
-router.get("/my-products", authUser, async (req, res) => {
-    const { _id } = req.user;
-    try {
-        const products = await Product.find({userId:_id});
-        if (!products) {
-            return res.status(404).json({error: true , message: "Product not found"});
-        }
-        res.status(200).json(products);
-    } catch (err) {
-        res.status(500).json({
-            error: true,
-            message: "Failed to fetch products",
-            detail: err.message
-        });
-    }
-})
+//Get product by genre
+router.get("/products", async (req, res) => {
+  const genre = req.query.genre;
 
-export default router
+  try {
+    const query = genre ? { "tags.title": genre } : {};
+    
+    const products = await Product.find(query)
+    res.status(200).json({
+      error: false,
+      products,
+    });
+} catch (err) {
+    res.status(500).json({
+      error: true,
+      message: "Failed to fetch products",
+      detail: err.message,
+    });
+  }
+});
+
+router.get("/my-products", authUser, async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const products = await Product.find({ userId: _id });
+    if (!products) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Product not found" });
+    }
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({
+      error: true,
+      message: "Failed to fetch products",
+      detail: err.message,
+    });
+  }
+});
+
+export default router;
