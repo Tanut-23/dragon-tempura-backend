d06ebab696db3eb811147844f93b1294ffbd40bc
supon26862 <supol1234594@gmail.com>
2025-05-10 23:09:15 +0700

    invite socket

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ import dotenv from "dotenv";
 import router from "./routes/route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { Server } from 'socket.io';
+import http from 'http';
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: 'http://localhost:5173', },
+});
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -21,17 +26,30 @@ app.use(express.json());
 const port = process.env.PORT;
 
 (async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log("Connected to Mongo database");
-    } catch (err){
-      console.error(`MongoDB connection error: ${err}`)
-      process.exit(1);
-    }
-  })();
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongo database");
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err}`)
+    process.exit(1);
+  }
+})();
+
+// Socket
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('oneBid', (dataAuctioneer) => {
+    io.emit('shareBid', dataAuctioneer);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
 
 app.use("", router);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
